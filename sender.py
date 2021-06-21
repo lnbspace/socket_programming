17a48c02eb0055b541d8e95edfa1e18a3a2e8c09
lnbspace <79511584+lnbspace@users.noreply.github.com>
2021-06-21 20:49:13 +0530

    Add files via upload

--- sender.py
+++ sender.py
@@ -1,12 +1,17 @@
 import socket 
+import time
 # import socket module /library to create 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 # what is my receiver address
 recv_addr=("127.0.0.1",9999)
 # Now i want to send message
-user_data=input("enter your message :- ")
-# converting data into ascii code
-newdata=user_data.encode('ascii')
-# now you can send data 
-s.sendto(newdata, recv_addr)
-print("your message has been sent..")
+while 2 > 1 :
+    user_data=input("enter your message :- ")
+    # converting data into ascii code
+    newdata=user_data.encode('ascii')
+    # now you can send data 
+    s.sendto(newdata, recv_addr)
+    time.sleep(2)
+    print("your message has been sent..")
+    # now waiting for reply 
+    print(s.recvfrom(10))
